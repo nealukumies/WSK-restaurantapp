5b95d22a239c5f40fefb122cf4110be3f168ab9e
nealukumies <nea.lukumies@metropolia.fi>
2025-03-27 10:25:22 +0200

    images for restaurants

--- js/renderRestaurants.js
+++ js/renderRestaurants.js
@@ -1,13 +1,27 @@
 export function renderRestaurants(restaurants) {
   const restContainer = document.querySelector('.restaurant-container');
 
-  for (const restaurant of restaurants) {
+  const foodImages = [
+    './img/food1.jpg',
+    './img/food2.jpg',
+    './img/food3.jpg',
+    './img/food4.jpg',
+    './img/food5.jpg',
+    './img/food6.jpg',
+    './img/food7.jpg',
+    './img/food8.jpg',
+    './img/food9.jpg',
+    './img/food10.jpg',
+  ];
+
+  for (let i = 0; i < restaurants.length; i++) {
+    const restaurant = restaurants[i];
     const restaurantDiv = document.createElement('div');
     restaurantDiv.setAttribute('class', 'restaurant');
 
     const img = document.createElement('img');
     img.setAttribute('class', 'restaurant-img');
-    img.setAttribute('src', 'https://placecats.com/350/250');
+    img.setAttribute('src', foodImages[i % foodImages.length]);
     img.setAttribute('alt', 'restaurant image');
 
     const h2 = document.createElement('h2');
