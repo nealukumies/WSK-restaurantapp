5cb3306d29b589c40c8010c73dd48b918936ff7f
nealukumies <nea.lukumies@metropolia.fi>
2025-04-15 13:48:59 +0300

    feat: add user authentication and profile page with API integration

---
+++ js/getUserDetails.js
@@ -0,0 +1,19 @@
+export async function getUserDetails(token) {
+  try {
+    const response = await fetch(
+      `https://media2.edu.metropolia.fi/restaurant/api/v1/users/${token}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer: ${token}`,
+        },
+      }
+    );
+    const jsonData = await response.json();
+    console.log(jsonData);
+    return jsonData;
+  } catch (error) {
+    console.log('Failed to get user details ' + error.message);
+  }
+}
--- js/login.js
+++ js/login.js
@@ -1,26 +1,31 @@
+import {getUserDetails} from './getUserDetails.js';
+
 const loginForm = document.getElementById('login-form');
 loginForm.addEventListener('submit', async (event) => {
   event.preventDefault();
 
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
-  const response = await fetch('http://localhost:3000/api/v1/auth/login', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({username, password}),
-  });
+  const response = await fetch(
+    'https://media2.edu.metropolia.fi/restaurant/api/v1/auth/login',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({username, password}),
+    }
+  );
   try {
     if (response.ok) {
       const result = await response.json();
 
       console.log('Login result:', result);
-      alert(
-        'Logged in as: ' + result.user.username + ' (' + result.user.role + ')'
-      );
+      alert('Logged in as: ' + result.data.username);
       localStorage.setItem('token', result.token);
       console.log('Token saved to localStorage:', result.token);
+      const user = await getUserDetails(result.token);
+      console.log('User details:', user);
 
       window.location.href = 'index.html';
     } else {
